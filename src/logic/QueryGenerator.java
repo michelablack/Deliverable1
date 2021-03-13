0de162fe1d9e4fbe0fcd4b10bb70cdf24e968297
michelablack <36127614+michelablack@users.noreply.github.com>
2021-03-13 12:06:57 +0100

    modified file

--- src/logic/QueryGenerator.java
+++ src/logic/QueryGenerator.java
@@ -11,11 +11,12 @@ public class QueryGenerator {
 	 * @return the String from which the Json is derived. 
 	 */
 	public static String getFixedTickets(String projName, Integer i, Integer j) {
-		
+		System.out.println("ciao");
 		return  "https://issues.apache.org/jira/rest/api/2/search?jql=project=%22"
                 + projName + "%22AND(%22status%22=%22closed%22OR"
                 + "%22status%22=%22resolved%22)AND%22resolution%22=%22fixed%22&fields=key,resolutiondate"
                 + i.toString() + "&maxResults=" + j.toString();
+		
 		
 	}
 	
